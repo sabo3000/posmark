fd20b17cfb4954a8159a88672599fbd459195b09
Sascha Bohrloch <sabo3000@saboweb.de>
2018-02-14 12:33:46 +0100

    pwa setup

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       <div className='App'>
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
-          <h1 className='App-title'>Welcome to React</h1>
+          <h1 className='App-title'>posmark - position marker webapp</h1>
         </header>
         <Main />
       </div>
