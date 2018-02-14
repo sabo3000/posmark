e2b08408383d355878902dc2d0271c22b398f539
Sascha Bohrloch <sabo3000@saboweb.de>
2018-02-14 17:54:07 +0100

    added api key

--- src/components/Map.js
+++ src/components/Map.js
@@ -10,10 +10,11 @@ const ContainerElem = styled.div`
 const LoaderElem = styled.div`
   height: 100%;
 `
+const apiKey = 'AIzaSyD1aeCOtB3_LSZQED20yRlqr5-HKFZWRIQ'
 
 const Map = compose(
   withProps({
-    googleMapURL: 'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places',
+    googleMapURL: 'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=' + apiKey,
     loadingElement: <LoaderElem />,
     containerElement: <ContainerElem />,
     mapElement: <div style={{ height: `100%` }} />
